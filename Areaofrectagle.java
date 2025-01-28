42537b6b432772a962221eae74950ac0331db9de
nandini-chakali <chakalinandini31@gmail.com>
2025-01-28 16:45:50 +0530

    Add files via upload

---
+++ Areaofrectagle.java
@@ -0,0 +1,9 @@
+public class Areaofrectagle {
+    public static void main(String[] args) {
+        int l=5;
+        float b=15f;
+        float area=l*b;
+        System.out.printf("Area of rectangle=%f",area);
+    }
+    
+}
